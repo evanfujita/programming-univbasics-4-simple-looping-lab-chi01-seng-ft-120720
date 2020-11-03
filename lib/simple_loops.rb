8a06cb67e5e0ee7c4a2deeaf308f9f0e479f1599
Evan Fujita <evanfujita@gmail.com>
2020-11-03 23:49:29 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -20,9 +20,8 @@ end
 
 def return_string_array(array)
   counter = 0 
-  new_array = []
   while counter > array.length do
-    new_array[counter] = array[counter].to_s
+    array[counter] = array[counter].to_s
     counter += 1
   end
   new_array
